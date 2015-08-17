340e9c29d6fff87ec467adacb08247aeb65b3d5a
Martin Fenner <martin.fenner@datacite.org>
2015-08-17 23:25:39 +0200

    fixed syntax error for node attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['nginx']['dir'] = '/etc/nginx'
 default['nginx']['log_dir'] = '/var/log/nginx'
 default['nginx']['worker_processes'] = 4
 default['nginx']['worker_connections'] = 768
-default['nginx']['subdomain'] = ['ec2']
+default['nginx']['subdomain'] = 'ec2'
 default['nginx']['applications'] = ['search']
