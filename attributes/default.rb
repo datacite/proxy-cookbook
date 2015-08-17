772655a1e28596608a801044f30e5b32890f4eb0
Martin Fenner <martin.fenner@datacite.org>
2015-08-17 23:06:45 +0200

    forward to internal DNS name

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default['nginx']['log_dir'] = '/var/log/nginx'
 default['nginx']['worker_processes'] = 4
 default['nginx']['worker_connections'] = 768
 default['nginx']['default_site_enabled'] = false
+default['nginx']['internal_name'] = ['ec2']
 default['nginx']['applications'] = ['search']
 
 default['ruby']['deploy_user'] = "vagrant"
