f8c74339b169efa50ed33a35cd33e01e4bd2e55e
Martin Fenner <martin.fenner@datacite.org>
2015-12-23 00:31:47 +0100

    fixed syntax error

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configures proxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.62"
+version           "1.0.63"
 
 # opscode cookbooks
 depends           "apt"
