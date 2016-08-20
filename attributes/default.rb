c917b2dd9ef2d59b8e29ccbc980156e03b384163
Martin Fenner <martin.fenner@datacite.org>
2016-08-20 18:23:45 +0200

    don't install mysql-5.6 client by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@ default['ruby']['deploy_group'] = "vagrant"
 default['ruby']['rails_env'] = "development"
 default['ruby']['enable_capistrano'] = false
 
-default['ruby']['packages'] = %w{ curl git mysql-client-5.6 python-software-properties software-properties-common zlib1g-dev nfs-common }
+default['ruby']['packages'] = %w{ curl git python-software-properties software-properties-common zlib1g-dev nfs-common }
 
 default["application"] = "proxy"
 
